9cbc4e4c1157613900b26b2bfed7f75b3faac8c7
maritzka <136319634+Martzka@users.noreply.github.com>
2025-01-09 23:32:39 +0800

    B_TahunKabisat.py

--- Tugas praktek demonstrasi/B_TahunKabisat.py
+++ Tugas praktek demonstrasi/B_TahunKabisat.py
@@ -1,5 +1,30 @@
-tahun = int(input("Masukkan tahun: "))
-if (tahun % 4 == 0 and (tahun % 100 != 0 or tahun % 400 == 0)):
-    print(f"{tahun} adalah tahun kabisat.")
-else:
-    print(f"{tahun} bukan tahun kabisat.")
+def tahun_kabisat(tahun):
+    if (tahun % 4 == 0 and (tahun % 100 != 0 or tahun % 400 == 0)):
+        return True
+    else:
+        return False
+
+def input_tahun():
+    tahun = input("Masukkan tahun: ")
+    if not tahun.isdigit():
+        print("Input tidak valid")
+        return None
+    return int(tahun)
+
+def main():
+    print("Welcome di perhitungan tahun kabisat :)")
+    tahun_array = []
+    jumlah_tahun = int(input("Berapa banyak tahun yang mau di cek ? "))
+    
+    for i in range(jumlah_tahun):
+        tahun = input_tahun()
+        if tahun is not None:
+            tahun_array.append(tahun)
+    
+    for tahun in tahun_array:
+        if tahun_kabisat(tahun):
+            print(f"{tahun} tahun kabisat.")
+        else:
+            print(f"{tahun} bukan tahun kabisat.")
+            
+main()
